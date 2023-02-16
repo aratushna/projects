fe2292f0c282fe9311cb7b2d27aa262f1e973310
aratushna <94103037+aratushna@users.noreply.github.com>
2023-02-16 10:30:46 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
-string[] digits= {"0","1","2","3","4","5","6","7","8","9"};
-string[] operators= {"/","-","*","+"};
-string[] digits= {"(",")"};
+List<string> digits = new List<string>() {"0","1","2","3","4","5","6","7","8","9"};
+List<string> operators = new List<string>() {"/","-","*","+"};
+List<string> paranthesis = new List<string>() {"(",")"};
+List<string> separators = new List<string>() {" "};
