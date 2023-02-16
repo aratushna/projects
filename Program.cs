7941606a9eabc92267e5e789a3c5d003fd1f0095
aratushna <94103037+aratushna@users.noreply.github.com>
2023-02-16 10:39:18 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,3 +4,16 @@ List<string> digits = new List<string>() {"0","1","2","3","4","5","6","7","8","9
 List<string> operators = new List<string>() {"/","-","*","+"};
 List<string> paranthesis = new List<string>() {"(",")"};
 List<string> separators = new List<string>() {" "};
+
+var union = digits.Concat(operators).Concat(paranthesis).Concat(separators).ToList();
+
+string? arithmetic_expression = "";
+int i_unn;
+
+/*
+arithmetic_expression = Console.Readline();
+foreach (char s in arithmetic expression)
+{
+Console.WriteLine(s);
+}
+*/
