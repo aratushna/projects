a4a59082de0be57815138ed40ec003e1cea4e983
aratushna <94103037+aratushna@users.noreply.github.com>
2023-02-22 09:41:43 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using System.Cellections;
+
+//making a list of all symbols used
 
 List<string> digits = new List<string>() {"0","1","2","3","4","5","6","7","8","9"};
 List<string> operators = new List<string>() {"/","-","*","+"};
 List<string> paranthesis = new List<string>() {"(",")"};
 List<string> separators = new List<string>() {" "};
 
-List<string> union = digits.Concat(operators).Concat(paranthesis).Concat(separators).ToList(); //all lists put together
+//making a dictionary of operators and their priorities
+
+Dictionary<string, int> operators_priorities = new Dictionary<string, int>()
+{
+    {"^", 3},
+    {"*", 2},
+    {"/", 2},
+    {"+", 1},
+    {"-", 1}
+} ;
+
+List<string> operators = operators_priorities.Keys.ToList();  //keys comes fist, so it's operators
+
+List<string> operators_paranthesis = operators.Concat(paranthesis).ToList();  //putting 2 lists together
+
+  List<string> union = digits.Concat(operators_paranthesis).ToList(); //all lists put together
 
 string? arithmetic_expression = "";
 int i_unn == 0;
